fa84dad3f4880cd78aae6e4a7c773cf4126ffd9f
Alex Morega <alex@grep.ro>
2016-05-03 13:04:46 +0300

    Document model

---
+++ maldini/models.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Document(models.Model):
+    path = models.CharField(max_length=4000, unique=True, db_index=True)
+    disk_size = models.BigIntegerField()
