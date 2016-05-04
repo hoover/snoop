b40acd57aa354199eb7cb93167d3dd7a20236e51
Alex Morega <alex@grep.ro>
2016-05-04 14:24:00 +0300

    'push' field

--- maldini/models.py
+++ maldini/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Document(models.Model):
     path = models.CharField(max_length=4000, unique=True, db_index=True)
     disk_size = models.BigIntegerField()
+    push = models.BooleanField(default=False)
 
 class FolderMark(models.Model):
     path = models.CharField(max_length=4000, unique=True, db_index=True)
