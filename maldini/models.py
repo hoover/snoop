f1c73b40c4e80de88af16c0f22886a0a48a82715
Alex Morega <alex@grep.ro>
2016-05-04 17:33:55 +0300

    'status' column

--- maldini/models.py
+++ maldini/models.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 from django.db import models
-
+from django.contrib.postgres.fields import JSONField
 
 class Document(models.Model):
     path = models.CharField(max_length=4000, unique=True, db_index=True)
     disk_size = models.BigIntegerField()
     push = models.BooleanField(default=False)
+    status = JSONField(default=dict)
 
 class FolderMark(models.Model):
     path = models.CharField(max_length=4000, unique=True, db_index=True)
