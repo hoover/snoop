599599a915a8a05ecdb76d3676f962bd352bbc96
Alex Morega <alex@grep.ro>
2016-05-04 14:33:34 +0300

    es status

---
+++ maldini/management/commands/status.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(settings.ELASTICSEARCH_URL)
+
+class Command(BaseCommand):
+
+    help = "ES status"
+
+    def handle(self, **options):
+        print 'count:', es.count()['count']
