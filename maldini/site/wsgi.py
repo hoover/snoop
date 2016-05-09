5132bf8b9c0e1605d5fb82449a37b79b0da67f86
Alex Morega <alex@grep.ro>
2016-05-09 20:42:29 +0300

    fix settings module name

--- maldini/site/wsgi.py
+++ maldini/site/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maldini.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maldini.site.settings")
 
 application = get_wsgi_application()
