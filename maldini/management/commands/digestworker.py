7a47bf75184d2a1e91a394b59086fb6a00ec8f59
Alex Morega <alex@grep.ro>
2016-05-26 23:45:34 +0200

    digest worker pulls tasks from queue

--- maldini/management/commands/digestworker.py
+++ maldini/management/commands/digestworker.py
@@ -1,16 +1,28 @@
 import simplejson as json
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from maldini import models
+from maldini import queue
 from maldini.digest import digest
 
 class Command(BaseCommand):
 
     help = "Run the `digest` worker"
 
-    def handle(self, **options):
-        for document in models.Document.objects.iterator():
-            data = digest(document)
-            models.Digest.objects.update_or_create(
-                id=document.id,
-                defaults={'data': json.dumps(data)},
-            )
+    def handle(self, verbosity, **options):
+        digest_queue = queue.get('digest')
+        while True:
+            with transaction.atomic():
+                task = digest_queue.get(block=False)
+                if not task:
+                    break
+
+                document = models.Document.objects.get(id=task.data['id'])
+                data = digest(document)
+                models.Digest.objects.update_or_create(
+                    id=document.id,
+                    defaults={'data': json.dumps(data)},
+                )
+
+                if verbosity > 1:
+                    print(document.id)
