f57ae376f8627cb79118227d67e3f51823901fe9
Alex Morega <alex@grep.ro>
2016-05-26 20:11:13 +0200

    delete unused models and fields

--- maldini/models.py
+++ maldini/models.py
@@ -5,22 +5,6 @@ from django.contrib.postgres.fields import JSONField
 class Document(models.Model):
     path = models.CharField(max_length=4000, unique=True, db_index=True)
     disk_size = models.BigIntegerField()
-    push = models.BooleanField(default=False)
-    status = JSONField(default=dict)
-
-class Failure(models.Model):
-    document = models.OneToOneField(Document, db_index=True,
-        on_delete=models.CASCADE)
-
-class Generation(models.Model):
-    document = models.OneToOneField(Document, db_index=True,
-        on_delete=models.CASCADE)
-    n = models.IntegerField()
-
-class Cache(models.Model):
-    document = models.OneToOneField(Document, db_index=True,
-        on_delete=models.CASCADE)
-    data = models.TextField()
 
 class Digest(models.Model):
     id = models.IntegerField(primary_key=True)
