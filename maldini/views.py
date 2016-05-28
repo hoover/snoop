71338c909df6d8d5b41fd2796ac7b4bcc4008ab9
Alex Morega <alex@grep.ro>
2016-05-28 16:04:22 +0200

    link to attachments

--- maldini/views.py
+++ maldini/views.py
@@ -6,6 +6,14 @@ from .digest import digest
 def document(request, id):
     doc = get_object_or_404(Document, id=id)
     data = digest(doc)
+
+    attachments = [{
+        'filename': a['filename'],
+        'id': doc.document_set.get(path=n).id,
+        'content_type': a['content_type'],
+    } for n, a in data.get('attachments', {}).items()]
+
     return render(request, 'document.html', {
         'data': data,
+        'attachments': attachments,
     })
