0a9a4d254d90e0429630a9a729f218e27204b4e6
Alex Morega <alex@grep.ro>
2016-05-29 22:01:37 +0300

    batch-create folder entries

--- maldini/management/commands/walkfolders.py
+++ maldini/management/commands/walkfolders.py
@@ -3,31 +3,43 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from maldini import models
 
+FOLDER = 'application/x-directory'
+BATCH_SIZE = 100
+
 class Command(BaseCommand):
 
     help = "Create fake folder entries"
 
     def handle(self, **options):
         root = Path(settings.MALDINI_ROOT)
-        seen = set()
+        seen = set(d.path for d in models.Document.objects.filter(
+            container_id=None, content_type=FOLDER))
         file_docs = models.Document.objects.filter(container_id=None)
-        n_created = n_existing = 0
+        n_created = 0
+
+        def create_batch():
+            models.Document.objects.bulk_create([
+                models.Document(path=p, disk_size=0, content_type=FOLDER)
+                for p in batch
+            ])
+            n = len(batch)
+            batch[:] = []
+            return n
+
+        batch = []
         for document in file_docs.iterator():
             current = Path(document.path).parent
             while str(current) != '.':
                 if current in seen:
                     break
-                _, created = models.Document.objects.get_or_create(
-                    path=current,
-                    disk_size=0,
-                    content_type='application/x-directory',
-                )
+                batch.append(current)
                 seen.add(current)
-                if created:
-                    n_created += 1
-                else:
-                    n_existing += 1
-
                 current = current.parent
 
-        print('folder entries:', n_created, 'created,', n_existing, 'existing')
+                if len(batch) >= BATCH_SIZE:
+                    n_created += create_batch()
+
+        if batch:
+            n_created += create_batch()
+
+        print('folder entries:', n_created, 'created,', len(seen), 'total')
