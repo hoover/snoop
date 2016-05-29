656cfe4dca986d8ed54ea780be34f92f65f77ee9
Alex Morega <alex@grep.ro>
2016-05-29 23:45:12 +0300

    display documents that failed to digest

--- maldini/views.py
+++ maldini/views.py
@@ -4,13 +4,14 @@ from .models import Document
 from .digest import files_in, digest, open_document
 
 def document(request, id):
+    up = None
+    attachments = []
+
     if id == '0':
-        up = None
         data = {
             'type': 'folder',
             'files': files_in(''),
         }
-        attachments = []
 
     else:
         doc = get_object_or_404(Document, id=id)
@@ -19,24 +20,29 @@ def document(request, id):
                 data = f.read()
                 return HttpResponse(data, content_type=doc.content_type)
 
-        data = digest(doc)
+        try:
+            data = digest(doc)
 
-        if data.get('type') == 'folder':
-            data['files'] = files_in(doc.path + '/')
+        except:
+            data = {'type': '-- ERROR --'}
 
-        attachments = [{
-            'filename': a['filename'],
-            'id': doc.document_set.get(path=n).id,
-            'content_type': a['content_type'],
-        } for n, a in data.get('attachments', {}).items()]
-
-        if doc.container:
-            up = doc.container.id
-        elif '/' in doc.path:
-            up_path = doc.path.rsplit('/', 1)[0]
-            up = Document.objects.get(container=None, path=up_path).id
         else:
-            up = 0
+            if data.get('type') == 'folder':
+                data['files'] = files_in(doc.path + '/')
+
+            attachments = [{
+                'filename': a['filename'],
+                'id': doc.document_set.get(path=n).id,
+                'content_type': a['content_type'],
+            } for n, a in data.get('attachments', {}).items()]
+
+            if doc.container:
+                up = doc.container.id
+            elif '/' in doc.path:
+                up_path = doc.path.rsplit('/', 1)[0]
+                up = Document.objects.get(container=None, path=up_path).id
+            else:
+                up = 0
 
     return render(request, 'document.html', {
         'up': up,
