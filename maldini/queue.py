6d2a2b46f7654f904f1cf6c9e76963eccb42be5e
Alex Morega <alex@grep.ro>
2016-05-30 22:55:44 +0300

    logic for our homegrown queue

--- maldini/queue.py
+++ maldini/queue.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
-from django.db import connection, transaction
+from django.db import connection, transaction, IntegrityError
 import pq
+from . import models
 
 @contextmanager
 def django_transaction(conn, **kwargs):
@@ -20,3 +21,50 @@ def _cleanup():
 
 def get(name):
     return pq.PQ(connection)[name]
+
+def put(queue, data, verbose=False):
+    try:
+        models.Job.objects.create(queue=queue, data=data)
+    except IntegrityError:
+        if verbose: print('job already exists, skipping:', queue, data)
+
+def iterate(queue, verbose=False, stop_first_error=False):
+    while True:
+        with transaction.atomic():
+            job = (
+                models.Job.objects
+                .select_for_update()
+                .filter(queue=queue)
+                .filter(started=False)
+                .first()
+            )
+
+            if job is None:
+                if verbose: print('No jobs available in', queue)
+                return
+
+            job.started = True
+            job.save()
+
+        if verbose: print(job.data)
+
+        @contextmanager
+        def work():
+            try:
+                yield job.data
+
+            except Exception:
+                if stop_first_error:
+                    job.started = False
+                    job.save()
+                    raise
+
+                elif verbose:
+                    print('ERR')
+
+            else:
+                # no error; delete the job
+                if verbose: print('OK')
+                job.delete()
+
+        yield work
