a55e0b49f5856309d83e8c2422887b20982a96d4
Alex Morega <alex@grep.ro>
2016-05-30 21:59:50 +0300

    extract job function

--- maldini/management/commands/digestworker.py
+++ maldini/management/commands/digestworker.py
@@ -5,13 +5,37 @@ from maldini import models
 from maldini import queue
 from maldini.digest import digest
 
+def perform_job(document):
+    data = digest(document)
+
+    for name, info in data.get('attachments', {}).items():
+        child, created = models.Document.objects.update_or_create(
+            container=document,
+            path=name,
+            defaults={
+                'disk_size': 0,
+                'content_type': info['content_type'],
+            },
+        )
+
+        if created:
+            queue.get('digest').put({'id': child.id})
+            if verbosity > 0:
+                print('new child', child.id)
+
+    models.Digest.objects.update_or_create(
+        id=document.id,
+        defaults={'data': json.dumps(data)},
+    )
+
+    queue.get('index').put({'id': document.id})
+
 class Command(BaseCommand):
 
     help = "Run the `digest` worker"
 
     def handle(self, verbosity, **options):
         digest_queue = queue.get('digest')
-        index_queue = queue.get('index')
         while True:
             task = digest_queue.get(block=False)
             if not task:
@@ -30,34 +54,13 @@ class Command(BaseCommand):
 
                 try:
                     with transaction.atomic():
-                        data = digest(document)
-                        data_json = json.dumps(data)
-
-                        for name, info in data.get('attachments', {}).items():
-                            child, created = models.Document.objects.update_or_create(
-                                container=document,
-                                path=name,
-                                defaults={
-                                    'disk_size': 0,
-                                    'content_type': info['content_type'],
-                                },
-                            )
-
-                            if created:
-                                digest_queue.put({'id': child.id})
-                                if verbosity > 0:
-                                    print('new child', child.id)
+                        perform_job(document)
 
                 except:
                     outcome = 'ERR'
 
                 else:
                     outcome = 'OK'
-                    models.Digest.objects.update_or_create(
-                        id=document.id,
-                        defaults={'data': data_json},
-                    )
-                    index_queue.put({'id': document.id})
                     err.delete()
 
                 if verbosity > 0:
