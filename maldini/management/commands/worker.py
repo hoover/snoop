a052fcff908d997a8f7011d43693044127fbe4f3
Alex Morega <alex@grep.ro>
2016-05-31 01:02:38 +0300

    a single 'worker' command

--- maldini/management/commands/worker.py
+++ maldini/management/commands/worker.py
@@ -4,18 +4,26 @@ from maldini import workers
 
 class Command(BaseCommand):
 
-    help = "Run the `index` worker"
+    help = "Run the `digest` worker"
 
     def add_arguments(self, parser):
+        parser.add_argument('queue')
         parser.add_argument('-x', action='store_true', dest='stop_first_error')
 
-    def handle(self, verbosity, stop_first_error, **options):
+    def handle(self, verbosity, queue, stop_first_error, **options):
+        if queue == 'digest':
+            worker = workers.digest
+        elif queue == 'index':
+            worker = workers.index
+        else:
+            raise ValueError("Unknown queue %r" % queue)
+
         queue_iterator = queues.iterate(
-            'index',
+            queue,
             verbose=verbosity > 0,
             stop_first_error=stop_first_error,
         )
 
         for work in queue_iterator:
             with work() as data:
-                workers.index(**data, verbose=verbosity>0)
+                worker(**data, verbose=verbosity>0)
