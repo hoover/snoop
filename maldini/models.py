38661069898a2460bdd07e68a66f8873670ae25d
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-06-18 14:30:40 +0300

    Add md5, sha1 to Document

--- maldini/models.py
+++ maldini/models.py
@@ -7,6 +7,9 @@ class Document(models.Model):
     content_type = models.CharField(max_length=100, blank=True)
     disk_size = models.BigIntegerField()
 
+    md5 = models.CharField(max_length=40, null=True)
+    sha1 = models.CharField(max_length=50, null=True)
+
     class Meta:
         # TODO: constraint does not apply to container=None rows
         unique_together = ('container', 'path')
