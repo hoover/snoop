512e5a645b2c1c347cdfa53170209f883432d492
Alex Morega <alex@grep.ro>
2016-06-20 00:02:16 +0300

    optimize the retrieval

--- scripts/rel1_docs.py
+++ scripts/rel1_docs.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from elasticsearch import Elasticsearch, helpers
 es = Elasticsearch(settings.ELASTICSEARCH_URL)
-query = {"filter": {"wildcard": {"text": "*"}}, "fields": []}
+query = {"filter": {"wildcard": {"text": "*"}}, "_source": false}
 with open('../rel1.txt', 'w', encoding='utf-8') as f:
-    for doc in helpers.scan(es, index='hoover-6', query=query):
+    for doc in helpers.scan(es, index='hoover-6', query=query, size=10000):
         print(doc['_id'], file=f)
+
+# should return 1231116 documents
