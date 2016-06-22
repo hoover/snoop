355fc313170ed54dcd95779f2df56bc2aff2dedc
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-06-22 18:17:45 +0300

    move document paths in constants

--- testsuite/test_emails.py
+++ testsuite/test_emails.py
@@ -1,9 +1,15 @@
+MAIL_PATH_MAPBOX = "eml-1-promotional/Introducing Mapbox Android Services - " \
+              "Mapbox Team <newsletter@mapbox.com> - 2016-04-20 1603.eml"
+MAIL_PATH_CODINGAME = "eml-1-promotional/New on CodinGame: Check it out! - " \
+                 "CodinGame <coders@codingame.com> - 2016-04-21 1034.eml"
+MAIL_PATH_CAMPUS = "eml-2-attachment/FW: Invitation Fontys Open Day 2nd " \
+                    "of February 2014 - Campus Venlo " \
+                    "<campusvenlo@fontys.nl> - 2013-12-16 1700.eml"
+
 def test_subject():
     from maldini import models, digest, emails
 
-    doc = models.Document(path='eml-1-promotional/'
-        'Introducing Mapbox Android Services - Mapbox Team '
-        '<newsletter@mapbox.com> - 2016-04-20 1603.eml')
+    doc = models.Document(path=MAIL_PATH_MAPBOX)
 
     with digest.open_document(doc) as f:
         email = emails.EmailParser(f)
