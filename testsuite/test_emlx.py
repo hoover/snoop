20b8ffddb965ebc8e40c386e233aafd78c4159aa
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-06-23 03:37:10 +0300

    more .emlx tests

--- testsuite/test_emlx.py
+++ testsuite/test_emlx.py
@@ -14,3 +14,20 @@ def test_subject():
     data = email.get_data()
 
     assert data['subject'] == "Re: promulgare lege"
+
+def test_tree_with_attachments():
+    email = get_emlx_for_path(PATH_EMLX_LEGE)
+    tree = email.get_tree()
+
+    headers = {'Subject', 'To', 'From', 'Date', 'Content-Type'}
+    assert headers.issubset(set(tree['headers'].keys()))
+
+    assert set(tree.keys()) == {'attachments', 'headers', 'parts'}
+    assert len(tree['attachments']) == 2
+    assert len(tree['parts']) == 4
+
+def test_get_data():
+    email = get_emlx_for_path(PATH_EMLX_LEGE)
+    attach = email.get_data().get('attachments')
+
+    assert len(attach) == 2
