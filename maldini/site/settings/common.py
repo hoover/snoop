3f4030a243189d1dd9b9209ed6da315cdcb8204e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-06-23 18:11:49 +0300

    replace default template backend with jinja2

--- maldini/site/settings/common.py
+++ maldini/site/settings/common.py
@@ -1,3 +1,6 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -25,16 +28,13 @@ ROOT_URLCONF = 'maldini.site.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+            'environment': 'maldini.jinja2.environment',
         },
     },
 ]
