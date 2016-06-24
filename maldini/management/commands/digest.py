56e18a07ebf805878c5a18b0a89e121cf5053995
Alex Morega <alex@grep.ro>
2016-06-24 18:04:22 +0300

    add "create children" flag to `digest` command

--- maldini/management/commands/digest.py
+++ maldini/management/commands/digest.py
@@ -1,7 +1,7 @@
 import simplejson as json
 from django.core.management.base import BaseCommand
 from maldini import models
-from maldini.digest import digest
+from maldini import digest
 
 class Command(BaseCommand):
 
@@ -9,7 +9,11 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('document_id', type=int)
+        parser.add_argument('-c', action='store_true', dest='children')
 
-    def handle(self, document_id, **options):
+    def handle(self, document_id, children, **options):
         document = models.Document.objects.get(id=document_id)
-        print(json.dumps(digest(document), indent=2))
+        data = digest.digest(document)
+        print(json.dumps(data, indent=2))
+        if children:
+            digest.create_children(document, data, verbose=True)
