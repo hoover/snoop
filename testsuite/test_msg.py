5ca351bf54aec6a4ac86590e7179cbd730ec4d1b
Alex Morega <alex@grep.ro>
2016-06-25 10:38:16 +0300

    fix .msg tests

--- testsuite/test_msg.py
+++ testsuite/test_msg.py
@@ -1,24 +1,28 @@
 from pathlib import Path
-from maldini import emails, digest
+from django.conf import settings
+from maldini import emails
 
-PATH_MSG_DISEARA = Path(r"msg-5-outlook/DISEARĂ-Te-așteptăm-la-"
-                        "discuția-despre-finanțarea-culturii.msg")
+PATH_MSG_DISEARA = ("msg-5-outlook/DISEARĂ-Te-așteptăm-la-"
+                    "discuția-despre-finanțarea-culturii.msg")
+
+def open_msg(path):
+    return emails.open_msg(Path(settings.MALDINI_ROOT) / path)
 
 def test_subject():
-    email = emails.open_msg(digest.doc_path(PATH_MSG_DISEARA))
+    email = open_msg(PATH_MSG_DISEARA)
     data = email.get_data()
 
     assert data['subject'] == "DISEARĂ: Te așteptăm la " \
                               "discuția despre finanțarea culturii"
 
 def test_text():
-    email = emails.open_msg(digest.doc_path(PATH_MSG_DISEARA))
+    email = open_msg(PATH_MSG_DISEARA)
 
     assert "Te așteptăm diseară de la 19:00 la Colivia" in email.get_text()
     assert "Întâlnirile Culturale CALEIDO: Despre finanțarea culturii" in email.get_text()
 
 def test_people():
-    email = emails.open_msg(digest.doc_path(PATH_MSG_DISEARA))
+    email = open_msg(PATH_MSG_DISEARA)
     data = email.get_data()
 
     assert "cosmin@caleido.ro" in data['from']
@@ -27,7 +31,7 @@ def test_people():
 
 
 def test_tree_without_attachments():
-    email = emails.open_msg(digest.doc_path(PATH_MSG_DISEARA))
+    email = open_msg(PATH_MSG_DISEARA)
     tree = email.get_tree()
 
     headers = {'Subject', 'To', 'From', 'Date', 'Content-Type'}
