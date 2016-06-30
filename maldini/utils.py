9a3afd8bf5b0c7726d954f1494cf7c44a21cd1df
Alex Morega <alex@grep.ro>
2016-06-30 13:01:44 +0300

    timeit helper

--- maldini/utils.py
+++ maldini/utils.py
@@ -4,3 +4,46 @@ def chunks(file, blocksize=65536):
         if not data:
             return
         yield data
+
+def timeit(name):
+    """ Measure aggregate time for a function or code block
+
+    .. sourcode:: python
+
+        @timeit('emails')
+        def parse_email(doc):
+            do_stuff()
+
+    .. sourcode:: python
+
+        emails_timer = timeit('emails')
+        for _ in range(10):
+            with emails_timer.cm():
+                do_stuff()
+    """
+    import sys, atexit, time
+    total = 0
+
+    @atexit.register
+    def report():
+        print("{}: {:.3f}".format(name, total), file=sys.stderr)
+
+    @contextmanager
+    def cm():
+        nonlocal total
+        t0 = time.time()
+        try:
+            yield
+        finally:
+            total += time.time() - t0
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            with cm():
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    decorator.cm = cm
+
+    return decorator
