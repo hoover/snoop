9167fc9a4eec110d533297f98bc3abe7f1d96c98
Alex Morega <alex@grep.ro>
2016-07-04 23:45:48 +0300

    show me diffs one by one

--- scripts/validate_email_cache.py
+++ scripts/validate_email_cache.py
@@ -1,5 +1,7 @@
 import json
 import random
+import subprocess
+import tempfile
 from maldini import models, emails
 
 EMAIL_CONTENT_TYPES = [
@@ -16,21 +18,31 @@ id_list = list(
 )
 id_pool = set(id_list)
 
+def diff(a, b, prefix):
+    with tempfile.NamedTemporaryFile(prefix=prefix) as t1:
+        with tempfile.NamedTemporaryFile(prefix=prefix) as t2:
+            t1.write(json.dumps(a, indent=2, sort_keys=True).encode('utf-8'))
+            t1.write(b'\n')
+            t1.flush()
+            t2.write(json.dumps(b, indent=2, sort_keys=True).encode('utf-8'))
+            t2.write(b'\n')
+            t2.flush()
+            subprocess.call(
+                'diff -u "{}" "{}" | less'.format(t1.name, t2.name),
+                shell=True,
+            )
+
 def check(batch):
     sample_ids = set(random.sample(id_pool, min([batch, len(id_pool)])))
-    errors = []
     for doc in models.Document.objects.filter(id__in=sample_ids).iterator():
         assert emails.is_email(doc)
         cache_row = models.EmailCache.objects.get(pk=doc.id)
         cached = json.loads(cache_row.value)
         correct = emails.raw_parse_email.no_cache(doc)
         if cached != correct:
-            errors.append(doc.id)
+            print('\n\n\n%d\n' % doc.id)
+            diff(cached, correct, str(doc.id)+'-')
     id_pool.difference_update(sample_ids)
-    return errors
 
-batch = 1000
-n = 0
 while id_pool:
-  n += 1
-  print(n * batch, check(batch))
+  check(10)
