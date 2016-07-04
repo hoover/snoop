5b309009605ec116fa87add11c197740cd9b21e4
Alex Morega <alex@grep.ro>
2016-07-04 14:29:38 +0300

    bulk api for queues

--- maldini/queues.py
+++ maldini/queues.py
@@ -51,3 +51,40 @@ def iterate(queue, verbose=False, stop_first_error=False, in_order=False):
                 job.delete()
 
         yield work
+
+def bulk(queue, batch, verbose=False):
+    while True:
+        with transaction.atomic():
+            query = (
+                models.Job.objects
+                .select_for_update()
+                .filter(queue=queue)
+                .filter(started=False)
+            )
+            jobs = list(query[:batch])
+
+            if not jobs:
+                if verbose: print('No jobs available in', queue)
+                return
+
+            job_ids = [job.id for job in jobs]
+            job_collection = models.Job.objects.filter(id__in=job_ids)
+
+            job_collection.update(started=True)
+
+        if verbose: print(len(job_ids), 'jobs')
+
+        @contextmanager
+        def work():
+            try:
+                yield [job.data for job in jobs]
+
+            except Exception:
+                print('ERR')
+
+            else:
+                # no error; delete the job
+                if verbose: print('OK')
+                job_collection.delete()
+
+        yield work
