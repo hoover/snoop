48a25dd9111e9c1279b2f2f998484bd6a9814047
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-08-08 22:10:09 +0300

    add decryption system and a test for it

---
+++ maldini/pgp.py
@@ -0,0 +1,57 @@
+import re
+import gnupg
+from django.conf import settings
+
+GPG = None
+
+def is_enabled():
+    return settings.MALDINI_GPG_HOME and settings.MALDINI_GPG_BINARY
+
+def _get_gpg():
+    global GPG
+    if is_enabled():
+        if not GPG:
+            GPG = gnupg.GPG(gnupghome=settings.MALDINI_GPG_HOME,
+                            gpgbinary=settings.MALDINI_GPG_BINARY)
+        return GPG
+    else:
+        raise RuntimeError("MALDINI_GPG_BINARY or MALDINI_GPG_HOME not set")
+
+class DecryptionError(RuntimeError):
+    pass
+
+def extract_pgp_block(content):
+    if isinstance(content, bytes):
+        content = content.decode('utf-8')
+    m = re.search(
+        r'-----BEGIN PGP MESSAGE-----[^-]+-----END PGP MESSAGE-----',
+        content, re.DOTALL)
+    if m:
+        return m.group(0)
+    else:
+        return None
+
+def contains_pgp_block(content):
+    if isinstance(content, bytes):
+        content = content.decode('utf-8')
+    m = re.search(r'-----BEGIN PGP MESSAGE-----', content)
+    return bool(m)
+
+def decrypt_pgp_block(content, passphrase=None):
+    text_block = extract_pgp_block(content)
+    if not text_block:
+        return content
+
+    gpg = _get_gpg()
+
+    if passphrase:
+        decrypt = gpg.decrypt(text_block, passphrase=passphrase)
+    else:
+        decrypt = gpg.decrypt(text_block)
+
+    if decrypt.ok:
+        return decrypt.data
+
+    raise DecryptionError(decrypt.status)
+
+
