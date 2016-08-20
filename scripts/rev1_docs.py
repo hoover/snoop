4293fd823155a302f75fbc86fa66daf81caf411e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-08-20 16:26:24 +0300

    remove old scripts

--- scripts/rev1_docs.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from elasticsearch import Elasticsearch, helpers
-es = Elasticsearch(settings.ELASTICSEARCH_URL)
-query = {"filter": {"wildcard": {"text": "*"}}, "_source": false}
-with open('../rel1.txt', 'w', encoding='utf-8') as f:
-    for doc in helpers.scan(es, index='hoover-6', query=query, size=10000):
-        print(doc['_id'], file=f)
-
-# should return 1231116 documents
