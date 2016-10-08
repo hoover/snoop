e1178a95244346e427c361561933277545a87a2c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-08 17:18:22 +0300

    implement json endpoint for documents

--- snoop/site/urls.py
+++ snoop/site/urls.py
@@ -5,6 +5,7 @@ from .. import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^doc/(?P<id>\d+)$', views.document),
+    url(r'^doc/(?P<id>\d+).json$', views.document_json),
     url(r'^(?s)doc/(?P<id>\d+)/raw/.*$', views.document_raw),
     url(r'^doc/(?P<id>\d+)/ocr/(?P<tag>[^/]+)/.*$', views.document_ocr),
     url(r'^(?s)doc/(?P<id>\d+)/eml/.*$', views.document_as_eml),
