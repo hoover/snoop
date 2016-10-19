bd3ca064b41f75532f5c7b96861180b3158d9128
Alex Morega <alex@grep.ro>
2016-10-19 20:09:33 +0300

    simplify example config

--- snoop/site/settings/example_local.py
+++ snoop/site/settings/example_local.py
@@ -10,14 +10,6 @@ SECRET_KEY = 'FIME: generate random string'
 
 SNOOP_ROOT = '/path/to/test/data'
 
-SNOOP_ELASTICSEARCH_URL = 'http://localhost:9200'
-SNOOP_ELASTICSEARCH_INDEX = 'hoover'
-
-SNOOP_TIKA_SERVER_ENDPOINT = 'http://localhost:9998'
-SNOOP_TIKA_FILE_TYPES = ['doc', 'pdf', 'xls', 'ppt']
-SNOOP_TIKA_MAX_FILE_SIZE = 32 * (2 ** 20)  # 32mb
-SNOOP_ANALYZE_LANG = False
-
 SNOOP_MSGCONVERT_SCRIPT = 'msgconvert'
 SNOOP_MSG_CACHE = 'path/to/msg/cache'
 
