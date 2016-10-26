3779d7adf5268fe72793883add6eac7baaf70972
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-26 22:49:51 +0200

    redo collection editing from the management command

--- snoop/management/commands/collection.py
+++ snoop/management/commands/collection.py
@@ -7,51 +7,51 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'slug',
+            'collection_slug',
             nargs='?',
             default=None,
             help="The slug of the collection to view/modify"
         )
         parser.add_argument(
             '--set-path',
-            dest='set_path',
+            dest='path',
             default=None,
             help='Modify the path for the collection'
         )
         parser.add_argument(
             '--set-title',
-            dest='set_title',
+            dest='title',
             default=None,
             help='Modify the user-readable title for the collection'
         )
         parser.add_argument(
             '--set-es-index',
-            dest='set_index',
+            dest='index',
             default=None,
             help='Modify the Elasticsearch index for the collection'
         )
         parser.add_argument(
             '--set-slug',
-            dest='set_slug',
+            dest='slug',
             default=None,
             help='Modify the slug for the collection'
         )
         parser.add_argument(
             '--set-description',
-            dest='set_description',
+            dest='description',
             default=None,
             help='Modify the single-line user-readable for the collection'
         )
 
-    def handle(self, slug, all, **options):
-        if not slug:
+    def handle(self, collection_slug, **options):
+        if not collection_slug:
             print_all_collections()
             return
 
         try:
-            collection = models.Collection.objects.filter(slug=slug).get()
+            collection = models.Collection.objects.filter(slug=collection_slug).get()
         except models.Collection.DoesNotExist:
-            print("Collection with slug", slug, "does not exist.")
+            print("Collection with slug", collection_slug, "does not exist.")
             return
 
         modify_collection(collection, **options)
@@ -72,27 +72,18 @@ def print_info_for_collections(collections):
 
 
 def modify_collection(collection, **options):
-    options = {k: options[k] for k in options.keys() if options[k] if not None}
     keys = {
-        'set_path',
-        'set_slug',
-        'set_index',
-        'set_title',
-        'set_description'
+        'path',
+        'slug',
+        'index',
+        'title',
+        'description'
     }
-    modified = set(options.keys()).intersection(keys)
-    if not modified:
-        return
-
-    if 'set_path' in options:
-        collection.path = options['set_path']
-    if 'set_slug' in options:
-        collection.slug = options['set_slug']
-    if 'set_index' in options:
-        collection.es_index = options['set_index']
-    if 'set_title' in options:
-        collection.title = options['set_title']
-    if 'set_description' in options:
-        collection.description = options['set_description']
-    collection.save()
+    modified = False
+    for key in keys:
+        if options[key] is not None:
+            setattr(collection, key, options[key])
+            modified = True
+    if modified:
+        collection.save()
 
