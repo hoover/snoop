73f0c3115563dc75c7d5f6ffb774ace8c35e043e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-26 22:58:03 +0200

    simplify data migration

--- snoop/migrations/0004_auto_20161004_0825.py
+++ snoop/migrations/0004_auto_20161004_0825.py
@@ -68,20 +68,18 @@ DELETE_ROOT = """
     path = '' and filename = '' and content_type = 'application/x-directory'
 """
 
-def count_documents(apps, schema_editor):
+def documents_exist(apps, schema_editor):
     db_alias = schema_editor.connection.alias
     Document = apps.get_model('snoop', 'Document')
     document_count = Document.objects.using(db_alias).count()
-    return document_count
+    return document_count > 0
 
 def insert_root(apps, schema_editor):
-    document_count = count_documents(apps, schema_editor)
-    if document_count > 0:
+    if documents_exist(apps, schema_editor):
         schema_editor.execute(INSERT_ROOT)
 
 def delete_root(apps, schema_editor):
-    document_count = count_documents(apps, schema_editor)
-    if document_count > 0:
+    if documents_exist(apps, schema_editor):
         schema_editor.execute(DELETE_ROOT)
 
 class Migration(migrations.Migration):
