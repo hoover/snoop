45af79a2226e195f1c25c87211921992b7b12671
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-11-13 16:20:52 +0200

    prompt for deletion of all the documents for a collection

--- snoop/management/commands/deletecollection.py
+++ snoop/management/commands/deletecollection.py
@@ -22,8 +22,14 @@ class Command(BaseCommand):
         num_documents = c.document_set.count()
         if num_documents > 0:
             print("The collection with slug", slug, "still has", str(num_documents), "available.")
-            print("Please delete those documents first, or move them to another collection.")
-            sys.exit(1)
+            print("Are you sure you want to delete the", slug, "collection with all its documents?")
+            print("This operation cannot be undone.")
+            confirm = input("Type YES to delete the {} collection".format(c.slug))
+            if confirm != 'YES':
+                print("You typed something other than 'YES', aborting.")
+                return
+            c.document_set.delete()
+            print("The", str(num_documents), "for collection", slug, "have been deleted.")
 
         c.delete()
 
